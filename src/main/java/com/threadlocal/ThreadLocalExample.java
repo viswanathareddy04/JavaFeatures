b9111969d8051b4916f685d091a487d004fb889f
viswanathareddy04 <injaviswanathreddy@gmail.com>
2022-12-11 13:09:23 -0500

    Functional Interface

---
+++ src/main/java/com/threadlocal/ThreadLocalExample.java
@@ -0,0 +1,27 @@
+package com.threadlocal;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadLocalExample {
+
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(10);
+    private static final ThreadLocal<SimpleDateFormat>  simpleDateFormatThreadLocal = ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd"));
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            executorService.submit( () -> {
+                String birthdate = getBirthdate(new Date());
+                System.out.println(birthdate);
+            });
+            
+        }
+    }
+
+    private static String getBirthdate(Date date) {
+        final SimpleDateFormat df = simpleDateFormatThreadLocal.get();
+        return df.format(date);
+
+    }
+}
